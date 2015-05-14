4f3dc06df3ef65cb36a7ece06d00faf9ba7e7a06
Arun Sood <arsood@gmail.com>
2015-05-14 12:40:20 -0700

    Altered directions

--- js/intermediate.js
+++ js/intermediate.js
@@ -13,7 +13,7 @@ function shuffleArray(array) {
 
 //Call the function "shuffleArray" passing in "animals" as the parameter
 
-//Write the beginning of a FOR loop from 1 to 12
+//Write the beginning of a FOR loop from 1 to 12 inclusive
 	document
 		.getElementById("c" + i)
 		.innerHTML = "<span class='animal'>" + animals[i - 1] + "</span>";
@@ -23,7 +23,7 @@ function shuffleArray(array) {
 
 //Define a variable called "spaces" and set it equal to a selector that selects all of the "td" elements
 
-//Write the beginning of a FOR loop from 0 to 11
+//Write the beginning of a FOR loop from 0 to 11 inclusive
 	spaces[i].addEventListener("click", function() {
 		if (this.classList.contains("success")) {
 			return false;
@@ -54,7 +54,7 @@ function resetArray() {
 function checkWin() {
 	if (pickedAnimals.length === 2) {
 		if (pickedAnimals[0].animal === pickedAnimals[1].animal) {
-			//Write the beginning of a FOR loop from 0 to 1
+			//Write the beginning of a FOR loop from 0 to 1 inclusive
 				document
 					.getElementById(pickedAnimals[i].id)
 					.classList
@@ -65,7 +65,7 @@ function checkWin() {
 		} else {
 			//Write an alert here to indicate to the user that the animals they selected didn't match
 
-			//Write the beginning of a FOR loop from 0 to 1
+			//Write the beginning of a FOR loop from 0 to 1 inclusive
 				document
 					.getElementById(pickedAnimals[i].id)
 					.childNodes[0]
