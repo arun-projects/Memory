491210b22195ca0dd3d428a93af5626c23222cff
Arun Sood <arsood@gmail.com>
2016-04-17 00:14:18 -0700

    Added solutions

--- js/beginner.js
+++ js/beginner.js
@@ -1,4 +1,5 @@
 //Define a variable here called "animals" that is equal to an array with 6 different animals repeated once each. You should have a total of 12 animals.
+var animals = ["Cat", "Dog", "Kangaroo", "Elephant", "Snake", "Parrot", "Cat", "Dog", "Kangaroo", "Elephant", "Snake", "Parrot"];
 
 function shuffleArray(array) {
 	for (var i = array.length - 1; i > 0; i--) {
@@ -9,21 +10,26 @@ function shuffleArray(array) {
 	}
 	
 	//Return the variable "array"
+	return array;
 }
 
 //Call the function "shuffleArray" passing in "animals" as the parameter
+shuffleArray(animals);
 
 //Write the beginning of a FOR loop from 1 to 12 inclusive
+for (var i = 1; i < 13; i++) {
 	document
 		.getElementById("c" + i)
 		.innerHTML = "<span class='animal'>" + animals[i - 1] + "</span>";
 }
 
 //Define a variable here called pickedAnimals and set it to a blank array
+var pickedAnimals = [];
 
 var spaces = document.getElementsByTagName("td");
 
 //Write the beginning of a FOR loop from 0 to 11 inclusive
+for (var i = 0; i < 12; i++) {
 	spaces[i].addEventListener("click", function() {
 		if (this.classList.contains("success")) {
 			return false;
@@ -44,6 +50,7 @@ var spaces = document.getElementsByTagName("td");
 
 			setTimeout(function() {
 				//Call the function "checkWin"
+				checkWin();
 			}, 500);
 		}
 	});
@@ -51,12 +58,14 @@ var spaces = document.getElementsByTagName("td");
 
 function resetArray() {
 	//Set the variable "pickedAnimals" to a blank array
+	pickedAnimals = [];
 }
 
 function checkWin() {
 	if (pickedAnimals.length === 2) {
 		if (pickedAnimals[0].animal === pickedAnimals[1].animal) {
 			//Write the beginning of a FOR loop from 0 to 1 inclusive
+			for (var i = 0; i < 2; i++) {
 				document
 					.getElementById(pickedAnimals[i].id)
 					.classList
@@ -64,10 +73,13 @@ function checkWin() {
 			}		
 
 			//Call the function "resetArray" here
+			resetArray();
 		} else {
 			//Write an alert here to indicate to the user that the animals they selected didn't match
+			alert("No match!");
 
 			//Write the beginning of a FOR loop from 0 to 1 inclusive
+			for (var i = 0; i < 2; i++) {
 				document
 					.getElementById(pickedAnimals[i].id)
 					.childNodes[0]
@@ -76,6 +88,7 @@ function checkWin() {
 			}
 
 			//Call the function "resetArray"
+			resetArray();
 		}
 	}
 }
